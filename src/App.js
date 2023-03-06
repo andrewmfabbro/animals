acff1f7c645103e4132a21284a99266f872386b4
Andrew <andrewmfabbro@gmail.com>
2023-03-06 15:16:43 -0500

    add animal images and a renderedAnimals to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import { useState } from "react";
+import AnimalShow from "./AnimalShow";
+
+function getRandomAnimals() {
+  const animals=['bird', 'cat', 'cow', 'dog', 'alligator', 'horse'];
+  return animals[Math.floor(Math.random() * animals.length)];
+}
 
 function App () {
-  const [count, setCount] = useState(0);
+  const[animals, setAnimals] = useState([]);
 
   const handleClick = () => {
-    setCount(count+1);
-  console.log('button clicked!')
+    //never update state directly
+    setAnimals([...animals, getRandomAnimals()]);
   };
+
+  const renderedAnimals = animals.map((animal, index) => {
+    return <AnimalShow type={animal} key={index}/>;
+  });
 
   //write a callback function inline or before to handle the click and call it.
   //<button onClick={() =>console.log('Button clicked!')}>
@@ -14,9 +24,8 @@ function App () {
   return (
     <div>
       <button onClick={handleClick}>Add Animal</button>
-      <div>
-        Number of animals: {count}
-      </div>
+      <div>{renderedAnimals}</div>
+      
     </div>
   );
 

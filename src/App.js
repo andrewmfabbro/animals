8687dfdcc55758eeac30c8e1e5c51f87e172aa29
Andrew <andrewmfabbro@gmail.com>
2023-03-06 13:41:16 -0500

    made a counter with useState to track animals

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import { useState } from "react";
+
 function App () {
+  const [count, setCount] = useState(0);
+
   const handleClick = () => {
-    console.log("Button was clicked!")
-  }
-  
+    setCount(count+1);
+  console.log('button clicked!')
+  };
+
+  //write a callback function inline or before to handle the click and call it.
+  //<button onClick={() =>console.log('Button clicked!')}>
+
   return (
     <div>
       <button onClick={handleClick}>Add Animal</button>
+      <div>
+        Number of animals: {count}
+      </div>
     </div>
   );
 
